fb8647f47b4b76e667b0040b3ad1fdd2b13d7038
Niket1997 <aniket.mahangare@uber.com>
2025-04-28 00:03:52 +0530

    working version with qdrant

--- backend/main.py
+++ backend/main.py
@@ -3,10 +3,17 @@
 # 1. /ingest
 # 2. /ask
 
+import uvicorn
 from app.api.endpoints import router
 from fastapi import FastAPI
+from mangum import Mangum
 
 app = FastAPI()
 
 # Include the router
 app.include_router(router)
+
+handler = Mangum(app)
+
+if __name__ == "__main__":
+   uvicorn.run(app, host="0.0.0.0", port=8080)
